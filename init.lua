e1dc3ff011f5a4ad7a955217b85370c00a0deb07
‮ <gxousos@gmail.com>
2022-12-09 16:22:44 +0200

    update

--- init.lua
+++ init.lua
@@ -10,7 +10,9 @@ local config = {
     remote         = "origin" , -- remote to use
     channel        = "nightly", -- "stable" or "nightly"
     version        = "latest" , -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
+    auto_quit      = false    , -- automatically quit the current session after a successful update
     pin_plugins    = nil      , -- nil, true, false (nil will pin plugins on stable only)
+    auto_reload    = false    , -- automatically reload and sync packer after a successful update
     skip_prompts   = false    , -- skip prompts about breaking changes
     show_changelog = true     , -- show the changelog after performing an update
   },
@@ -31,6 +33,37 @@ local config = {
         ["Add Cursor Down"] = '<C-A-j>',
         ["Add Cursor Up"]   = '<C-A-k>',
       },
+
+      set_cursor_to_find = function(ref)
+        local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+        for line_number, ln in ipairs(lines) do
+          local start_index, end_index = string.find(ln, ref)
+          if start_index and end_index then
+            vim.api.nvim_win_set_cursor(0, {line_number, start_index})
+            break  -- Stop the loop because we found the search string
+          end
+        end
+      end,
+
+      go_to_markdown_ref = function ()
+        local cursor = vim.api.nvim_win_get_cursor(0)
+        local line = vim.api.nvim_buf_get_lines(0, cursor[1]-1, cursor[1] , false)[1]
+
+        for match in string.gmatch(line, "%(([^'%)]+)") do
+          local start_pos = string.find(line, match)
+          local end_pos = start_pos + string.len(match)
+          local link = ''
+          if cursor[2] +1 >= start_pos and cursor[2] < end_pos then
+            local rel_path = string.gsub(vim.api.nvim_buf_get_name(0), "(.+/)(.+)", "%1")
+            vim.api.nvim_command(':edit ' .. rel_path .. string.match(match, "^([^#]*)"))
+            vim.api.nvim_command("m\'")
+            link = string.match(match,'#(.*)$')
+            if link then
+              vim.api.nvim_command(":call set_cursor_to_find('" .. link .. "')") -- GOD PLEASE WHY
+            end
+          end
+        end
+      end,
   },},
 
 
@@ -67,23 +100,25 @@ local config = {
     end,
 
     plugins = { -- enable or disable extra plugin highlighting
-      aerial                = true ,
+      indent_blankline      = true ,
+      symbols_outline       = false,
+      highlighturl          = true ,
+      bufferline            = true ,
+      beacon                = false,
       notify                = true ,
+      aerial                = true ,
+      cmp                   = true ,
+      hop                   = false,
       rainbow               = true ,
+      vimwiki               = false,
       telescope             = true ,
       dashboard             = true ,
-      bufferline            = true ,
-      highlighturl          = true ,
-      indent_blankline      = true ,
-      ["nvim-web-devicons"] = true ,
-      ["neo-tree"         ] = true ,
-      ["which-key"        ] = true ,
-      ["nvim-tree"        ] = false,
-      hop                   = false,
-      vimwiki               = false,
-      beacon                = false,
+      treesitter            = true ,
       lightspeed            = false,
-      symbols_outline       = false,
+      ["neo-tree"]          = true ,
+      ["which-key"]         = true ,
+      ["nvim-tree"]         = false,
+      ["nvim-web-devicons"] = true ,
   },},
 
 
@@ -115,24 +150,13 @@ local config = {
       {'Mofiqul/vscode.nvim'                    },
       {'nvim-treesitter/nvim-treesitter-context'},
       {'svermeulen/vim-yoink'                   },
+      {'AndrewRadev/switch.vim'                 },
       {'svermeulen/vim-subversive'              },
       {'folke/trouble.nvim'                     },
       {'godlygeek/tabular'                      }, -- ALIGN <leader>a | https://stackoverflow.com/questions/5436715/how-do-i-align-like-this-with-vims-tabular-plugin
-      -- {'mfussenegger/nvim-dap'           ,
-      --   config = function()
-      --     local dap, dapui = require("dap"), require("dapui")
-      --     dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open () end
-      --     dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
-      --     dap.listeners.before.event_exited    ["dapui_config"] = function() dapui.close() end
-      --   end}, -- python -m pip install debugpy
-      {"Pocco81/DAPInstall.nvim"                },
       {"nvim-telescope/telescope-dap.nvim"      },
-      -- {'renerocksai/telekasten.nvim'     , config = function() require('telekasten'           ).setup { home = _G.ZK_HONE_PATH } end},
       {'folke/twilight.nvim'             , config = function() require('twilight'             ).setup { context=50 }             end}, -- TODO: FIX
-      {'theHamsta/nvim-dap-virtual-text' , config = function() require("nvim-dap-virtual-text").setup()                          end},
-      {"mfussenegger/nvim-dap-python"    , config = function() require('dap-python'           ).setup(_G.PYTHON_PATH)            end},
       {'petertriho/nvim-scrollbar'       , config = function() require("scrollbar"            ).setup()                          end},
-      -- {"rcarriga/nvim-dap-ui"            , config = function() require("dapui"                ).setup()                          end , requires = {"nvim-dap"}},
       {'iamcco/markdown-preview.nvim'    , run    = function() vim.fn["mkdp#util#install"]()                                     end},
       {'Shatur/neovim-ayu'               , config = function()
         -- local utils = require "default_theme.utils"
@@ -278,6 +302,8 @@ local config = {
     local opts = { silent=true }
 
     map.set('n', '<Leader>tt', 'a<C-R>=strftime("%Y-%m-%d %I:%M:%S %p")<CR><Esc>', { desc = 'print time'} )
+    map.set('n', '<Leader>i', 'a<C-R>=strftime("__%Y%m%d%I%M%S%p")<CR><Esc>', { desc = 'print time'} ) -- TODO: store it into register
+    map.set('n', '<Leader>I', 'a<C-R>=strftime("__%Y%m%d%I%M%S%p")<CR><Esc>', { desc = 'print time'} )
     map.set('n', '";', '"_')
 
     map.set('i', '<C-S>'   , '<C-o>:w<cr>'              )
@@ -334,16 +360,7 @@ local config = {
     -- map.set('n', "<Tab>"   , ">>"              , bufopts)
     -- map.set('n', "<S-Tab>" , "<<"              , bufopts) -- windows issue
 
-    -- map.set("n", "<F5>"      , ":lua require('dap').continue()<cr>"         , opts)
-    -- map.set("n", "<F10>"     , ":lua require('dap').step_over()<cr>"        , opts)
-    -- map.set("n", "<F11>"     , ":lua require('dap').step_into()<cr>"        , opts)
-    -- map.set("n", "<F12>"     , ":lua require('dap').step_out()<cr>"         , opts)
     map.set("n", "<leader>b" , ":lua require('dap').toggle_breakpoint()<cr>", { desc = 'Breakpoint Toggle'} )
-    -- map.set("n", "<leader>B" , ":lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<cr>"      , { desc = 'Breakpoint Condition'} )
-    -- map.set("n", "<leader>lp", ":lua require('dap').set_breakpoint(nil, nil, vim.fn.input('Logpoint message: '))<cr>", opts)
-    -- map.set("n", "<leader>rp", ":lua require('dap').repl.open()<cr>"        , opts)
-    -- map.set("n", "<leader>RR", ":lua require('dap').run_last()<cr>"         , opts)
-    -- map.set("n", "<leader>XX", ":lua require('dap').terminate()<cr>"        , { desc = 'Terminate DAP'})
 
 
     vim.api.nvim_command("nnoremap <expr> j v:count ? (v:count > 5 ? \"m'\" . v:count : '') . 'j' : 'gj'"                      )
@@ -358,7 +375,11 @@ local config = {
     --Tabularize /(.*)
     -- vim.api.nvim_command('xnoremap p "_dP' ) -- mehh
 
-    vim.api.nvim_command('set conceallevel=1')
+    vim.api.nvim_command('set conceallevel=3') -- au FileType markdown setl conceallevel=0
+    vim.api.nvim_command('au BufRead,BufNewFile *.md nnoremap <buffer> gf :call go_to_markdown_ref()<cr>') -- https://www.reddit.com/r/vim/comments/yu49m1/rundont_run_vim_command_based_on_current_file/
+
+    -- vim.api.nvim_command("nnoremap <c-a> :if !switch#Switch({'reverse': 0}) <bar> exe 'normal! <c-a>' <bar> endif<cr>") -- https://github.com/AndrewRadev/switch.vim/pull/41
+    -- vim.api.nvim_command("nnoremap <c-x> :if !switch#Switch({'reverse': 1}) <bar> exe 'normal! <c-x>' <bar> endif<cr>")
 
     -- vim.api.nvim_command("let g:python3_host_prog = 'C:\\Users\\gxous\\AppData\\Local\\Programs\\Python\\Python39\\python.exe'")
     -- map.set('n', 'N', '#') -- IF NOT ALREADY SLASH SEARCH (i think i can do this with lua and states)
