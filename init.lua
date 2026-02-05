e45c200fe03bb35698472b7a089c8939720c3036
‮ <gxousos@gmail.com>
2026-02-05 10:56:47 +0200

    ADDED: `snacks.nvim` indent chunk + animate

--- init.lua
+++ init.lua
@@ -125,7 +125,25 @@ local plugins = {
 
 
   {"ray-x/lsp_signature.nvim", event = "BufRead", config = function() require("lsp_signature").setup(({hint_prefix='• '})) end,}, -- hints
-  {"folke/snacks.nvim", opts = { dashboard = { preset = { header = table.concat({
+  {"folke/snacks.nvim", opts = {
+    indent = {
+      enabled = true,
+      animate = { enabled = true, },
+      chunk = {
+        -- when enabled, scopes will be rendered as chunks, except for the
+        -- top-level scope which will be rendered as a scope.
+        enabled      = true,
+        only_current = false, -- only show chunk scopes in the current window
+        priority     = 1,
+        hl           = "SnacksIndentChunk", ---@type string | string[] hl group for chunk scopes
+        char = {
+          corner_top    = "╭",
+          corner_bottom = "╰",
+          horizontal    = "─",
+          vertical      = "│",
+          arrow         = ">",
+    },},},
+    dashboard = { preset = { header = table.concat({
     "             \\                                      [            ",
     "              @                 ⟡                  ╢             ",
     "      /       ╣▒                                  ]▒       \\     ",
@@ -142,7 +160,8 @@ local plugins = {
     " ¬U                                                            jU",
     "  C                                                            j ",
     "   L                                                          ]  ",
-  },"\n"),},},},},
+    },"\n"),},},
+  },},
 
   -- Colorschemes
   {"szorfein/darkest-space", lazy = true },
